39ee97957335936bd483a69257249e9292e6a42c
24yp01 <24scarlett10@sookmyung.ac.kr>
2025-08-19 14:46:50 +0900

    Login/Sign In 상단바 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+// src/App.jsx
 import './App.css';
 
 function App() {
@@ -13,15 +14,17 @@ function App() {
             <div className="btn green" />
           </div>
         </div>
+
         <div className="login-body">
           <h1 className="title">peach<br />planet</h1>
+
           <div className="form">
             <label className="input-label" htmlFor="id-input">id</label>
-            <input className="glass-input" type="text" id="id-input" placeholder=""/>
+            <input className="glass-input" type="text" id="id-input" placeholder="" />
 
             <label className="input-label" htmlFor="pw-input">pw</label>
-            <input className="glass-input" type="password" id="pw-input" placeholder=""/>
-           </div>
+            <input className="glass-input" type="password" id="pw-input" placeholder="" />
+          </div>
         </div>
       </div>
 
@@ -35,18 +38,32 @@ function App() {
             <div className="btn green" />
           </div>
         </div>
+
         <div className="main-body">
           <button className="aboutus-button">aboutus</button>
+
           <p className="desc">
             welcome to peachplanet<br />
             this is a diary you can record your day<br />
             with 3D characters<br />
             click to make your own emotion character
           </p>
+
           <div className="nav-buttons">
-            <div className="nav-btn">◀</div>
-            <div className="nav-btn">♡</div>
-            <div className="nav-btn">▶</div>
+            <div className="nav-item">
+              <div className="nav-btn">◀</div>
+              <span className="nav-label">join</span>
+            </div>
+
+            <div className="nav-item">
+              <div className="nav-btn">♡</div>
+              <span className="nav-label">&nbsp;</span> {/* 빈 라벨로 높이 맞춤 */}
+            </div>
+
+            <div className="nav-item">
+              <div className="nav-btn">▶</div>
+              <span className="nav-label">sign in</span>
+            </div>
           </div>
         </div>
       </div>
